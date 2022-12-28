de2650b84c9ae37f2acc46a9744363d36c6a54ed
Clement Vasseur <cvasseur@fundrise.com>
2022-12-28 18:19:47 -0500

    day 25 2022 part 1

--- src/Days/Day25.hs
+++ src/Days/Day25.hs
@@ -1,6 +1,5 @@
-module Days.Day25 (runDay) where
+module Days.Day25 where
 
-{- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -14,26 +13,57 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
-{- ORMOLU_ENABLE -}
+import Options.Applicative ((<|>), Alternative (empty))
 
 runDay :: R.Day
-runDay = R.runDay inputParser partA partB
+runDay = R.runDay snafuListParser partA partB
 
 ------------ PARSER ------------
-inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+snafuParser :: Parser Int
+snafuParser = do
+    snafuDigits <- map snafuDigitToInt . reverse <$> snafuCharParser
+    return $ sum $ zipWith (*) snafuDigits (map (5^) [0..])
+
+snafuCharParser :: Parser [Char]
+snafuCharParser = many1' (satisfy (inClass "-012=")) <* (endOfLine <|> endOfInput)
+
+snafuDigitToInt :: Char -> Int
+snafuDigitToInt '0' = 0
+snafuDigitToInt '1' = 1
+snafuDigitToInt '2' = 2
+snafuDigitToInt '-' = -1
+snafuDigitToInt '=' = -2
+snafuDigitToInt x = error $ "unknow character " ++ show x
+
+snafuListParser :: Parser [Int]
+snafuListParser = many1' snafuParser <* endOfInput
+
+intToSnafu :: Int -> [Char]
+intToSnafu 0 = ""
+intToSnafu number = do
+    let (division, remainder) = quotRem number 5
+    if remainder > 2 
+        then
+            do
+                let snafuRemainder = case remainder of
+                                        3 -> "="
+                                        4 -> "-"
+                                        _ -> error "don't know what to do for remainder " ++ show remainder
+                
+                intToSnafu (division + 1) <> snafuRemainder
+        else
+            intToSnafu division <> show remainder
 
 ------------ TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+
+-- Part A:
+-- "20=022=21--=2--12=-2"
+-- (0.000008s)
+partA :: [Int] -> [Char]
+partA = intToSnafu . sum
 
 ------------ PART B ------------
-partB :: Input -> OutputB
+partB :: [Int] -> [Int]
 partB = error "Not implemented yet!"
